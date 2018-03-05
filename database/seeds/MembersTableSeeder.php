1f6cc12a4bab4155111bf04705611ad76732b9c3
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-03-05 15:57:27 +0700

    added dummy record in seeder

---
+++ database/seeds/MembersTableSeeder.php
@@ -0,0 +1,338 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MembersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Member::insert([
+			[
+				'name'  => 'Dinova Widytianto',
+				'email' => 'dinovaprogram@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Dwi Krisiyani',
+				'email' => 'Dwi.krisiyani25@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Indhun Khonyfatul Ulfah',
+				'email' => 'khonyfatul.ulfah@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Sindi Kristiani',
+				'email' => 'Sindikristiani39@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'laila aprina bastian pamungkas',
+				'email' => 'laila.aprina45@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Wulan apriliya Rahayuningsih',
+				'email' => 'wulanapriliya104@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Dea Sandi Pratama',
+				'email' => 'Deasandipratama10@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Fendi Zulvika Aprianto',
+				'email' => 'fendi.zulvika@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Artha Kusuma Bhaskara',
+				'email' => 'akbhaskara@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Riki Widianto',
+				'email' => 'riki.widiant@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Hanif Pambudi',
+				'email' => 'pambudihanif@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Arum Mawar Sari',
+				'email' => 'arummawar473@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'desi ayu anita',
+				'email' => 'desianita814@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Diyas khoiriyah',
+				'email' => 'Diyaskhoir123@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Ludvita Isnandar',
+				'email' => 'isnalud06@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Ani Krisnawati',
+				'email' => 'Anikrisnawati30@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Siti Faida Lailatul Murtazaqoh',
+				'email' => 'faidalailatul6@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Fitroh Ambar Nilawati',
+				'email' => 'ambarnilawati2@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Indah Sari',
+				'email' => '7indahsari@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Yana Nur Laili',
+				'email' => 'yananurlaili@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Fardhan Ardhi Ramadhan',
+				'email' => 'fa.dsg.id@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Evan Nanda Satriya',
+				'email' => 'evanandasatria@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Ashyfa Restu Wulandari',
+				'email' => 'ashyfaxrpl@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Moh.Tri Putra',
+				'email' => 'Moh.triputraXIrpl@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Yunarno',
+				'email' => 'Yunarno07@gmail. Com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Ayu Lestari',
+				'email' => 'princessayulestari99@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Lailatul Muniroh',
+				'email' => 'lailatulmuniroh182@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Arlina fingkiani',
+				'email' => 'Arlinaingkiani@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Ayu Mutoharoh',
+				'email' => 'ayumutoharoh7@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Riska yuni sri mulyati',
+				'email' => 'riskayunis99@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Ervina Nuryulistin',
+				'email' => 'ervina15nuryulistin@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Erna Erfiana',
+				'email' => 'erfianaerna22@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Siti Sholihah',
+				'email' => 'sitisholihah461@gmail.Com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Dewi muslikhah',
+				'email' => 'dewiIkha19@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Sunthi Purba',
+				'email' => 'shuntypurba@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Vina Desnia Bagdianingrum',
+				'email' => 'vinabagdianingrum1@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Reno Rahardian',
+				'email' => 'e.g.rahardyan@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Tri Witanto',
+				'email' => 'triwitanto81@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Aurelsa Puspitasari',
+				'email' => 'aurelsa07@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Galuh Sekar Pramesti',
+				'email' => 'sekarpramesti46@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Wahyu Dwi Setiawan',
+				'email' => 'wahyudwisetiawan04@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Adip Olga Pangestu',
+				'email' => 'adiptrt22@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Canda Dayu Agan Jukiva',
+				'email' => 'Candadayuaganj@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Ahmad Dani Febrianto',
+				'email' => 'ahmaddfebrianto@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Prendy Eko Prasetyo',
+				'email' => 'Prendyekoprasetyo@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Dinar singodimejo',
+				'email' => 'dinarsingo1@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Fega Setyawan',
+				'email' => 'Fegasetyawan15@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Dimas Achmad Indria Sapta Nugraha',
+				'email' => 'dimasachmadnugraha@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Anas Shofyan Martunis Fateh',
+				'email' => 'shofyananasmf@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Moh Krisna Alwi',
+				'email' => 'alwikrisna@gmail.con',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Maryon Jerycho Titioka',
+				'email' => 'Jerichomario92@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Theo Remalia Ayal',
+				'email' => 'ayaltheo@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Charmeilita Joma Diece Makatita',
+				'email' => 'charmeilitamakatita@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Fernando Kreid Lopuhaa',
+				'email' => 'fernandokreid10@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Merfin Lopulalan',
+				'email' => 'merfinlopulalan@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Fransisco Seccio Brenard Thorion',
+				'email' => 'fransiscothorion@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'samuel Soumokil',
+				'email' => 'samueljosua29@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Valdy Soumokil',
+				'email' => 'valdysou02@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Rivaldo Stuard Jeey Sapiya',
+				'email' => 'r.sapiya@yahoo.co.id',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Nicarlos.B.Manusiwa',
+				'email' => 'ayentanabe@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'M Zakky Mubarok Arromadhon',
+				'email' => 'mzakkymubarok20@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Alam Kibar Romadhon',
+				'email' => 'alam.adhon22@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'Fitria Risqi Amelia',
+				'email' => 'fitriarisqiamelia87@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			],
+			[
+				'name'  => 'sustiyaningsih',
+				'email' => 'sustiyaningsih1212@gmail.com',
+				'created_at' => \Carbon\Carbon::now('Asia/Jakarta')
+			]
+
+        ]);
+    }
+}
